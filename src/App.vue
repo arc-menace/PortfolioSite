39bb6f24643ed7ebb4e2729ae7e7fee88881eb68
Joseph Hahn <jmhahn0@gmail.com>
2024-09-03 17:25:23 -0500

    interim commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import Navbar from './components/Navbar/Navbar.vue';
+import Navbar from './components/navbar/Navbar.vue';
 import Home from './pages/Home.vue';
 import Projects from './pages/Projects.vue';
 import Resume from './pages/Resume.vue';
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+</script>
+
+<template>
+    <div class="project-card" :class="{ 'dark': theme.global.current.value.dark }">
+
+    </div>
+</template>
+
+<style scoped>
+.project-card {
+    border: 1px solid #000;
+    border-radius: 8px;
+    padding: 16px;
+    margin: 16px 0;
+}
+
+.project-card.dark {
+    border: 1px solid #fff;
+}
+</style>
